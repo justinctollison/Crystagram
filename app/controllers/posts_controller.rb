e88e87dea1da6590149ea5261ae104c801b87e4f
justinctollison <justinctollison@gmail.com>
2022-05-04 22:34:50 -0700

    Removed some comments, cleaning up code.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,6 @@ class PostsController < ApplicationController
         end
     end
 
-    #REFACTOR
     def update
         if user_post.update(post_params)
             render json: post
@@ -28,7 +27,6 @@ class PostsController < ApplicationController
         end
     end
 
-    #REFACTOR
     def destroy
         user_post.delete
         head :no_content
